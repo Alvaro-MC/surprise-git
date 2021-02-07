1975e9859fde26a0fb2ef06672118034758fd71d
Alvaro-MC <alvaro.chico@grupomcperu.com>
2021-02-06 21:21:10 -0500

    Conexion
    Actualizacion de la conexion PDO a GCP

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,11 +1,12 @@
 <?php
-$dbHost = '75.102.57.89';
-$dbName = 'qhgawllm_bdsurprise';
-$dbUser = 'qhgawllm';
-$dbPass = 'FD,9#MF&.CtP';
+$dbHost = '/cloudsql/prueba-surprise:us-central1:bd-surprise-mc';
+$dbName = 'bdsurprise';
+$dbUser = 'alvaro-mc';
+$dbPass = 'alvaro2021';
 
 try {
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    //$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $pdo = new PDO("mysql:$dbHost", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e) {
     echo $e->getMessage();
