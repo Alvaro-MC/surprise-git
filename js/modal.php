c1931070a3b25ff9735a4330f631921b7e34b500
Alvaro-MC <alvaro.chico@grupomcperu.com>
2021-02-06 22:13:25 -0500

    cnx
    cnx

--- js/modal.php
+++
@@ -1,8 +0,0 @@
-<?php
-$queryResult = $pdo->prepare("SELECT id_panel,stock_videos as stock from panel where id_panel=:panel");
-$queryResult->execute([
-    'panel' => $_POST['number']
-]);
-$pan = $queryResult->fetch(PDO::FETCH_ASSOC);
-return $pan['stock_videos'];
-?>
--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,12 +1,12 @@
 <?php
-$dbHost = '/cloudsql/prueba-surprise:us-central1:bd-surprise-mc';
-$dbName = 'bdsurprise';
-$dbUser = 'alvaro-mc';
-$dbPass = 'alvaro2021';
+$dbHost = '75.102.57.89';
+$dbName = 'qhgawllm_bdsurprise';
+$dbUser = 'qhgawllm';
+$dbPass = 'FD,9#MF&.CtP';
 
 try {
-    //$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
-    $pdo = new PDO("mysql:$dbHost", $user, $password);
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    //$pdo = new PDO("mysql:$dbHost", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e) {
     echo $e->getMessage();
