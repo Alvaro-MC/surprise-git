623ccc68fc5d6dbb3913a3b7fd2be2fbe3aff2f7
Alvaro-MC <alvaro.chico@grupomcperu.com>
2021-02-08 13:32:15 -0500

    RR
    RR

--- home.php
+++ home.php
@@ -60,6 +60,53 @@ if (!empty($_POST)) {
 
 <body>
 
+    <!-- PopUp de Panel 1 -->
+    <div class="modal fade" id="ventanaPanelSurprise" tabindex="-1" role="dialog" aria-labelledby="">
+        <div class="modal-dialog modal-dialog-centered modal-lg justify-content-center" role="document">
+            <div class="modal-content mc-panel p-0">
+                <div class="modal-body p-0">
+                    <div class="container-fluid cont-modal-panel p-0">
+                        <div class="row row-up">
+                            <div class="col-12">
+                                <div id="panel-img" class="sombra-panel" style="background-color: #f5e2d3;">
+                                    <script>
+                                        var panel = localStorage.getItem("panel")
+                                        switch (localStorage.getItem("panel")) {
+                                            case 1:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/portico-huanchaco.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 2:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/paradero-laesperanza02.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 3:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/paradero-laesperanza02.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 4:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/portico-mall.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 5:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/paradero-elgolf.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 6:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/paradero-larco.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 7:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/portico-realplaza.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                            case 8:
+                                                document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" src="css/video/videos-paneles/portico-elporvenir.mp4" playsinline autoplay muted loop></video>')
+                                                break;
+                                        }
+                                    </script>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php require_once 'popups.php'; ?>
 
     <!-- Cabecera -> Video - NavBar - SliderNotas -->
@@ -156,7 +203,7 @@ if (!empty($_POST)) {
                     <script>
                         var p = localStorage.getItem("panel")
                     </script>
-                    <button type="button" class="btn-surprise" onclick=previsualizarPanel(pa)>
+                    <button type="button" class="btn-surprise" data-bs-toggle="modal" data-bs-target="#ventanaPanelSurprise">
                         <i class="far fa-play-circle blanco"></i>
                     </button>
                 </div>
@@ -447,27 +494,6 @@ if (!empty($_POST)) {
                                         }
                                     </script>
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- PopUp de Panel 1 -->
-    <div class="modal fade" id="ventanaPanelSurprise" tabindex="-1" role="dialog" aria-labelledby="">
-        <div class="modal-dialog modal-dialog-centered modal-lg justify-content-center" role="document">
-            <div class="modal-content mc-panel p-0">
-                <div class="modal-body p-0">
-                    <div class="container-fluid cont-modal-panel p-0">
-                        <div class="row row-up">
-                            <div class="col-12">
-                                <div id="panel-img" class="sombra-panel" style="background-color: #f5e2d3;">
-                                    <script>
-                                        document.write('<video id="video-popup-panel" class="video-ini" style="width: 100%;" playsinline autoplay muted loop></video>')
-                                    </script>
                                 </div>
                             </div>
                         </div>
